9b57dd241a763818420cb826ac51341972be946b
FES001 <firdavs7794948@gmail.com>
2024-08-27 22:00:44 +0500

    Almost done.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const textInput = document.getElementById('textInput');
 const shortenBtn = document.getElementById('submitBtn');
 const outputContainer = document.querySelector('.output-container');
+const errorMessage = document.querySelector('.error-message');
 
 shortenBtn.addEventListener('click', shortenLink);
 
 async function shortenLink(e) {
     e.preventDefault();
 
-    const response = await fetch('https://cleanuri.com/api/v1/shorten');
-
-    console.log(response);
+    /* const response = await fetch(`https://cleanuri.com/api/v1/shorten`, {
+        method: 'POST'
+    });
+    const jsonDataCameFromResponse = await response.json();
+    console.log(jsonDataCameFromResponse); */
     outputContainer.innerHTML += `
     <div class="output">
         <span class="output-left" id="linkEnteredText">https://www.frontendmentor.io</span>
@@ -20,3 +23,7 @@ async function shortenLink(e) {
     </div>
     `;
 };
+
+fetch('https://cleanuri.com/api/v1/shorten/result_url', {
+    method: 'POST'})
+    .then(response => console.log(response))
