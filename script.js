ae55fcf884da79ba54b3cbbfdacfe6fdfe5d2bc7
FES001 <firdavs7794948@gmail.com>
2024-08-27 21:44:55 +0500

    Something

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ shortenBtn.addEventListener('click', shortenLink);
 async function shortenLink(e) {
     e.preventDefault();
 
-    const response = await fetch('https://cleanuri.com/api/v1/shorten/google.com')
+    const response = await fetch('https://cleanuri.com/api/v1/shorten');
+
     console.log(response);
+    outputContainer.innerHTML += `
+    <div class="output">
+        <span class="output-left" id="linkEnteredText">https://www.frontendmentor.io</span>
+        <span class="output-right">
+        <span id="shortenedLinkText">https://rel.ink/k4IKyk</span>
+        <button id="copyBtn" class="copy-button">Copy</button>
+        </span>
+    </div>
+    `;
 };
